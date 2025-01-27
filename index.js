c6d8da47091f2a82ea50c56635082470483c5890
Johnny Walker <jwalker@designsforhealth.com>
2025-01-27 12:32:28 -0500

    fix: allow setting statsd prefix

--- index.js
+++ index.js
@@ -3,6 +3,41 @@
 const { spawn } = require('child_process')
 const { join } = require('path')
 
-spawn(process.execPath, ['-r', join(__dirname, 'monitor.js')].concat(process.argv.slice(2)), {
+function printUsage() {
+  console.log('Usage: node-monitor [options] <script> [script-args]')
+  console.log('Options:')
+  console.log('  --statsd-prefix, -p <prefix>  Set the statsd prefix')
+}
+
+function parseArgs() {
+  const args = process.argv.slice(2)
+  if (args.length === 0) {
+    printUsage()
+    process.exit(1)
+  }
+  if (args[0] === '--help' || args[0] === '-h') {
+    printUsage()
+    process.exit(0)
+  }
+
+  // check if the first argument is a flag to set the statsd prefix
+  if (args[0] === '--statsd-prefix' || args[0] === '-p') {
+    return {
+      statsdPrefix: args[1],
+      restArgs: args.slice(2),
+    }
+  }
+  return {
+    statsdPrefix: null,
+    restArgs: args,
+  }
+}
+
+const { statsdPrefix, restArgs } = parseArgs()
+if (statsdPrefix) {
+  process.env.STATSD_PREFIX = statsdPrefix
+}
+
+spawn(process.execPath, ['-r', join(__dirname, 'monitor.js')].concat(restArgs), {
   stdio: 'inherit',
 })
--- monitor.js
+++ monitor.js
@@ -1,18 +1,26 @@
 const top = require('process-top')()
 const StatsD = require('hot-shots')
 
+function getStatsDPrefix() {
+  const envVar = process.env.STATSD_PREFIX
+  // trim leading and trailing dots
+  const trimmedEnvVar = envVar?.replace(/^\.+|\.+$/g, '') ?? ''
+  return trimmedEnvVar ? `nodemon.${trimmedEnvVar}.` : 'nodemon.'
+}
+
 const client = new StatsD({
-  globalTags: { env: process.env.NODE_ENV },
+  prefix: getStatsDPrefix(),
 })
 
 setInterval(function () {
   const cpu = top.cpu()
   const memory = top.memory()
   const delay = top.delay()
+  client.increment('my_counter')
   client.gauge('cpu', cpu.percent)
   client.gauge('rss', memory.rss)
   client.gauge('heapTotal', memory.heapTotal)
   client.gauge('heapUsed', memory.heapUsed)
   client.gauge('total', memory.total)
   client.gauge('delay', delay)
-}, 1000).unref()
+}, 200).unref()
